1d6bd159fd5028637793484ff0ff90bfadcdc48a
NastyaArt <artemchuk.nastya@gmail.com>
2018-05-17 20:01:17 +0300

    add simple example

---
+++ src/main/java/org/abcd/examples/ArrayInit/gen/ArrayInitVisitor.java
@@ -0,0 +1,25 @@
+// Generated from D:/Nastya/Java/MatVecCompiler/array-example/src/main/antlr4/org/abcd/examples/ArrayInit\ArrayInit.g4 by ANTLR 4.7
+package org.abcd.examples.ArrayInit.gen;
+import org.antlr.v4.runtime.tree.ParseTreeVisitor;
+
+/**
+ * This interface defines a complete generic visitor for a parse tree produced
+ * by {@link ArrayInitParser}.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public interface ArrayInitVisitor<T> extends ParseTreeVisitor<T> {
+	/**
+	 * Visit a parse tree produced by {@link ArrayInitParser#init}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitInit(ArrayInitParser.InitContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ArrayInitParser#value}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitValue(ArrayInitParser.ValueContext ctx);
+}
